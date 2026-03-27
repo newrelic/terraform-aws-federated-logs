3070a77c79d2539dd26669105c81053de81fcc93
Himanshu Rai <hrai@newrelic.com>
2026-03-27 16:00:30 +0530

    adding lifecycle ignore params

--- modules/federated_logs_partition/main.tf
+++ modules/federated_logs_partition/main.tf
@@ -10,6 +10,18 @@ resource "aws_glue_catalog_table" "iceberg_table" {
   name          = each.key
   database_name = var.glue_catalog_db_name
 
+  lifecycle {
+    ignore_changes = [
+      # Prevent TF from fighting with Athena/Iceberg over these dynamic keys
+      parameters["previous_metadata_location"],
+      parameters["metadata_location"],
+      parameters["current-snapshot-id"],
+      parameters["current-snapshot-timestamp-ms"],
+      parameters["current-snapshot-summary"],
+      parameters["snapshot-count"]
+    ]
+  }
+  
   open_table_format_input {
     iceberg_input {
       metadata_operation = "CREATE"
