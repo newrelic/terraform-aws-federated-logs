bfe2514c506c535d4e97948e4955aae37d23209c
Aashirwadjain <aashirwadjain@newrelic.com>
2026-04-13 15:07:52 +0530

    fix(setup-module): fix s3 bucket deletion error for setup module

--- modules/federated_logs_setup_resource/main.tf
+++ modules/federated_logs_setup_resource/main.tf
@@ -2,6 +2,17 @@ resource "aws_s3_bucket" "this" {
   bucket = local.setup_naming_prefix
 }
 
+resource "null_resource" "empty_bucket_on_destroy" {
+  triggers = {
+    bucket_id = aws_s3_bucket.this.id
+  }
+
+  provisioner "local-exec" {
+    when    = destroy
+    command = "aws s3 rm s3://${self.triggers.bucket_id} --recursive || true"
+  }
+}
+
 resource "aws_glue_catalog_database" "this" {
   name        = lower(replace(local.setup_naming_prefix, "-", "_"))
   description = "Glue database containing NR resources for federated logs"
