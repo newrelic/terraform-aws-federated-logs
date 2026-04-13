5c3b01816717de6cb25d3ca376e7f34e1ebdb610
Aashirwadjain <aashirwadjain@newrelic.com>
2026-04-13 15:22:41 +0530

    fix(modules): update provider block

--- modules/federated_logs_setup_resource/providers.tf
+++ modules/federated_logs_setup_resource/providers.tf
@@ -5,9 +5,9 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
   }
-}
-
-provider "aws" {
-  region = "us-east-1"
 }
