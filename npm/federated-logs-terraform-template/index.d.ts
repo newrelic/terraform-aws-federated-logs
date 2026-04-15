335cf9eef636f6b339f1eb477677da069540277e
satya-nr <psatyanarayana@newrelic.com>
2026-04-15 14:42:28 +0530

    feat: publish Terraform template as npm package for UI consumption

---
+++ npm/federated-logs-terraform-template/index.d.ts
@@ -0,0 +1,3 @@
+export declare const FEDERATED_LOGS_TF_VERSION: string;
+export declare const FEDERATED_LOGS_TF_PLACEHOLDER_SETUP_NAME: '__SETUP_NAME__';
+export declare const FEDERATED_LOGS_TF_TEMPLATE: string;
